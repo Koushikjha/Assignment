6884227566b8c8ecdba1fba0827085b3767738eb
Koushikjha <koushik70422@gmail.com>
2025-10-14 23:12:34 +0530

    Create Count Word.java

---
+++ String II/Count Word.java
@@ -0,0 +1,9 @@
+package StringII;
+
+public class CountWord {
+    public static void main(String[] args) {
+        String s="A string is a stream of characters";
+        String[] tokens=s.split(" ");
+        System.out.println(tokens.length);
+    }
+}
