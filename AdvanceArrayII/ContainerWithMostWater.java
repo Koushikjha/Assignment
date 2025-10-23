6dac866c3aa7a5017ef9c29b48f43950c6f7a92d
Koushikjha <koushik70422@gmail.com>
2025-10-23 22:51:40 +0530

    Create ContainerWithMostWater.java

---
+++ AdvanceArrayII/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package AdvancedArrayII;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+
+    }
+    public int maxArea(int[] height) {
+        int n=height.length;
+        int v=0,w=0;
+        int s=0,e=n-1;
+        while(s<e){
+            if(height[s]>height[e]){
+                v=height[e]*(e-s);
+            }else{
+                v=height[s]*(e-s);
+            }
+            w=Math.max(v,w);
+            if(height[s]>height[e]){
+                e--;
+            }else{
+                s++;
+            }
+        }
+        return w;
+    }
+}
