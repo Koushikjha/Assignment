7772a90844af786a39e0c6ef6f9ae5e8c3bf8eb5
Koushikjha <koushik70422@gmail.com>
2025-10-23 23:57:53 +0530

    Add files via upload

---
+++ BinaryTreeIII/RightSideView.java
@@ -0,0 +1,32 @@
+package BinaryTreeIII;
+
+public class RightSideView {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val=val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
+        }
+        return find(root,targetSum);
+    }
+    public boolean find(TreeNode node,int sum){
+        if(node==null){
+            return false;
+        }
+        if(node.left==null && node.right==null && sum==node.val){
+            return true;
+        }
+
+        return find(node.left,sum-node.val) || find(node.right,sum-node.val);
+    }
+}
