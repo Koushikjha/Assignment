949557160d1bda9d7092f5c807cb5fc2858f41d0
Koushikjha <koushik70422@gmail.com>
2025-10-31 15:30:55 +0530

    Create LongestPalindromeSubsequences.java

---
+++ Dynamic Programming VII/LongestPalindromeSubsequences.java
@@ -0,0 +1,26 @@
+package DynamicProgrammingVII;
+
+public class PalindromicSubstrings {
+    public static void main(String[] args) {
+
+    }
+    public int countSubstrings(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int len1=find(s,i,i);
+            int len2=find(s,i,i+1);
+            sum+=len1+len2;
+        }
+        return sum;
+    }
+    public int find(String p,int s,int e){
+        int c=0;
+        while(s>=0 && e<p.length() && p.charAt(s)==p.charAt(e)){
+            c++;
+            s--;
+            e++;
+        }
+        return c;
+    }
+}
