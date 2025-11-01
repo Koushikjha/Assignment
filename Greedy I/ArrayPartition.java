efd8e8bf89001ccd435bbd25bb1c0b88722ba928
Koushikjha <koushik70422@gmail.com>
2025-11-01 19:26:36 +0530

    Create ArrayPartition.java

---
+++ Greedy I/ArrayPartition.java
@@ -0,0 +1,17 @@
+package GreedyI;
+import java.util.*;
+public class ArrayPartition {
+    public static void main(String[] args) {
+
+    }
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        int i=0;
+        while(i<nums.length){
+            sum+=nums[i];
+            i+=2;
+        }
+        return sum;
+    }
+}
