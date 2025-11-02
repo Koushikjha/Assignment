4e44a0c2ea23cdc56a4e6eadf8e25c71d2c9641f
Koushikjha <koushik70422@gmail.com>
2025-11-03 00:52:45 +0530

    Create IntersectionOfArrays.java

---
+++ HashMap III/IntersectionOfArrays.java
@@ -0,0 +1,30 @@
+package HashMapIII;
+import java.util.*;
+public class IntersectionOfArrays {
+    public static void main(String[] args) {
+
+    }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        Set<Integer> set=new HashSet<>();
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] ans=new int[set.size()];
+        int k=0;
+        for(int num : set){
+            ans[k++]=num;
+        }
+        return ans;
+    }
+}
