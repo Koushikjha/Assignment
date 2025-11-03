27d37b269474d87def5d4959b577ba185d7df69d
Koushikjha <koushik70422@gmail.com>
2025-11-03 13:23:23 +0530

    Create AllAnagramInString.java

---
+++ Sum Up HashMap/AllAnagramInString.java
@@ -0,0 +1,27 @@
+package SumUpHashMap;
+import java.util.*;
+public class AllAnagramInString {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] scount=new int[26];
+        int[] pcount=new int[26];
+        List<Integer> res=new ArrayList<>();
+        for(char c : p.toCharArray()){
+            pcount[c-'a']++;
+        }
+        int window=p.length();
+        for(int i=0;i<s.length();i++){
+            scount[s.charAt(i)-'a']++;
+            if(i>=window){
+                scount[s.charAt(i-window)-'a']--;
+            }
+            if(Arrays.equals(pcount,scount)){
+                res.add(i-window+1);
+            }
+
+        }
+        return res;
+    }
+}
