abe453a058cbf32bd8e4830b303990ed2daf9d97
Koushikjha <koushik70422@gmail.com>
2025-11-03 11:03:40 +0530

    Create BullsAndCows.java

---
+++ HashMap-SlidingWindow/BullsAndCows.java
@@ -0,0 +1,26 @@
+package HashMapSlidingWindow;
+import java.util.*;
+public class BullsAndCows {
+    public static void main(String[] args) {
+
+    }
+    public String getHint(String secret, String guess) {
+        int bulls=0,cows=0;
+        int[] s=new int[10];
+        int[] c=new int[10];
+        for(int i=0;i<guess.length();i++){
+            int x=secret.charAt(i);
+            int y=guess.charAt(i);
+            if(x==y){
+                bulls++;
+            }else{
+                s[x-'0']++;
+                c[y-'0']++;
+            }
+        }
+        for(int i=0;i<10;i++){
+            cows+=Math.min(s[i],c[i]);
+        }
+        return bulls+"A"+cows+"B";
+    }
+}
