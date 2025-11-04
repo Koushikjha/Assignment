805da95e680de320b35171c640a082c02c168399
Koushikjha <koushik70422@gmail.com>
2025-11-04 10:58:57 +0530

    Create InplaceHeapSort.java

---
+++ Priority Queue II/InplaceHeapSort.java
@@ -0,0 +1,30 @@
+package PriorityQueueII;
+
+public class InPlaceHeapSort {
+    public static void main(String[] args) {
+
+    }
+    public void heap(int[] arr,int n){
+        for(int i=n/2-1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+    }
+    public void heapify(int[] arr,int n,int i){
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<n && arr[left]>arr[largest]){
+            largest=left;
+        }
+        if(right<n && arr[right]>arr[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,n,largest);
+        }
+
+    }
+}
