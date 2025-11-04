7ae73e9e6175ef5571129c87dfec34402e4e957f
Koushikjha <koushik70422@gmail.com>
2025-11-04 15:03:33 +0530

    Create CountPrimes.java

---
+++ Number Theory/CountPrimes.java
@@ -0,0 +1,25 @@
+package NumberTheory;
+//Sieve Of Erastosthenes
+public class CountPrimes {
+    public static void main(String[] args) {
+
+    }
+    public int countPrimes(int n) {
+        boolean[] vis=new boolean[n];
+        if(n<2){
+            return 0;
+        }
+        vis[0]=true;
+        vis[1]=true;
+        int c=0;
+        for(int i=2;i<n;i++){
+            if(!vis[i]){
+                c++;
+                for(int j=1;j*i<n;j++){
+                    vis[j*i]=true;
+                }
+            }
+        }
+        return c;
+    }
+}
