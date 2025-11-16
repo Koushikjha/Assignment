df90acb17f57a5d8eb0a3d79aa0b4cea1b7cd59e
Koushikjha <koushik70422@gmail.com>
2025-11-16 11:46:47 +0530

    Update KthSmallestElement.java

--- Priority Queue II/KthSmallestElement.java
+++ Priority Queue II/KthSmallestElement.java
@@ -1,4 +1,20 @@
-package PriorityQueueII;
+package PriorityQ.PriorityQueueII;
+
+import java.util.*;
 
 public class KthSmallestElement {
+    public static int findKthSmallest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int n : nums) {
+            pq.offer(n);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(findKthSmallest(arr, k));
+    }
 }
